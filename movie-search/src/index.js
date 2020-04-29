41b8f1de0a3ab46efd373a27d0a0e7d922b691f5
Ihar <mauler1987@gmail.com>
2020-04-29 16:09:33 +0300

    feat: footer and imdb rating on cards

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -204,17 +204,24 @@ class Movie {
   getMovieItem() {
     const movieItem = document.createElement("div");
     movieItem.classList = "movie-item";
-    movieItem.innerHTML = `<div class="movie-item-poster" style="background-image:url(${
+    movieItem.innerHTML = `<div class="movie-item__poster" style="background-image:url(${
       this.Poster !== "N/A"
         ? this.Poster
         : "https://renderman.pixar.com/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png"
     })">
-                <!--div class="movie-item-heart ${
-                  myMoviesProxy.includes(this) ? "movie-item-heart-visible" : ""
+                <!--div class="movie-item__heart ${
+                  myMoviesProxy.includes(this) ? "movie-item__heart_visible" : ""
                 }"><i class="fa fa-heart" aria-hidden="true"></i></div-->
             </div>
-            <div class="movie-item-title">${this.Title}</div>
-            <div class="movie-item-year">${this.Year}</div>`;
+            <div class="movie-item__title">${this.Title}</div>
+            <div class="movie-item__year">${this.Year}</div>
+            <div class="movie-modal__rating-item">
+            <div class="movie-modal__rating-item_icon" style="background-image: url(https://cdn4.iconfinder.com/data/icons/socialmediaicons_v120/16/imdb.png);"></div>
+            <div class="movie-modal__rating-item_score">${
+              this.Ratings[0] != null
+                ? this.Ratings[0].Value
+                : "N/A"
+            }</div>`;
 
     movieItem.addEventListener("click", () => {
       sectionContainer.classList.add("section-container-blurred");
@@ -227,14 +234,14 @@ class Movie {
 
   getMovieModal() {
     const movieModal = document.createElement("div");
-    movieModal.classList = "movie-modal-detail content-wrapper";
-    movieModal.innerHTML = `<div class="movie-modal-poster" style="background-image:url(${
+    movieModal.classList = "movie-modal__detail content-wrapper";
+    movieModal.innerHTML = `<div class="movie-modal__poster" style="background-image:url(${
       this.Poster !== "N/A"
         ? this.Poster
         : "https://renderman.pixar.com/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png"
     })">
-                <div class="movie-modal-close"><i class="fa fa-arrow-left" aria-hidden="true"></i></div>
-                <div class="movie-modal-heart"><i class="fa ${
+                <div class="movie-modal__close"><i class="fa fa-arrow-left" aria-hidden="true"></i></div>
+                <div class="movie-modal__heart"><i class="fa ${
                   myMoviesProxy.find(element => {
                     return element.imdbID === this.imdbID;
                   }) !== undefined
@@ -242,38 +249,38 @@ class Movie {
                     : "fa-heart-o"
                 }" aria-hidden="true"></i></div>
             </div>
-            <div class="movie-modal-body">
-                <div class="movie-modal-title">${this.Title}</div>
-                <div class="movie-modal-year">${this.Year}</div>
-                <div class="movie-modal-director">${this.Director}</div>
-                <div class="movie-modal-rating-container">
-                    <div class="movie-modal-rating-item">
-                        <div class="movie-modal-rating-item-icon" style="background-image: url(https://staticv2-4.rottentomatoes.com/static/images/icons/CF_16x16.png);"></div>
-                        <div class="movie-modal-rating-item-score">${
+            <div class="movie-modal__body">
+                <div class="movie-modal__title">${this.Title}</div>
+                <div class="movie-modal__year">${this.Year}</div>
+                <div class="movie-modal__director">${this.Director}</div>
+                <div class="movie-modal__rating-container">
+                    <div class="movie-modal__rating-item">
+                        <div class="movie-modal__rating-item_icon" style="background-image: url(https://staticv2-4.rottentomatoes.com/static/images/icons/CF_16x16.png);"></div>
+                        <div class="movie-modal__rating-item_score">${
                           this.Ratings[1] != null
                             ? this.Ratings[1].Value
                             : "N/A"
                         }</div>
                     </div>
-                    <div class="movie-modal-rating-item">
-                        <div class="movie-modal-rating-item-icon" style="background-image: url(https://cdn4.iconfinder.com/data/icons/socialmediaicons_v120/16/imdb.png);"></div>
-                        <div class="movie-modal-rating-item-score">${
+                    <div class="movie-modal__rating-item">
+                        <div class="movie-modal__rating-item_icon" style="background-image: url(https://cdn4.iconfinder.com/data/icons/socialmediaicons_v120/16/imdb.png);"></div>
+                        <div class="movie-modal__rating-item_score">${
                           this.Ratings[0] != null
                             ? this.Ratings[0].Value
                             : "N/A"
                         }</div>
                     </div>
                 </div>
-                <div class="movie-modal-plot">${this.Plot}</div>
+                <div class="movie-modal__plot">${this.Plot}</div>
             </div>`;
     movieModal
-      .querySelector(".movie-modal-close")
+      .querySelector(".movie-modal__close")
       .addEventListener("click", () => {
         modalContainer.innerHTML = "";
         sectionContainer.classList.remove("section-container-blurred");
       });
     movieModal
-      .querySelector(".movie-modal-heart")
+      .querySelector(".movie-modal__heart")
       .addEventListener("click", event => {
         this.changeMyMovieItem(event, this, this.movieItem);
       });
@@ -281,7 +288,7 @@ class Movie {
   }
 
   changeMyMovieItem(event, movie, movieItem) {
-    this.movieItemHeart = movieItem.querySelector(".movie-item-heart");
+    this.movieItemHeart = movieItem.querySelector(".movie-item__heart");
     if (!myMoviesProxy.includes(movie)) {
       myMoviesProxy.push(movie);
       event.target.classList.remove("fa-heart-o");
